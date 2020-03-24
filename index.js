9606c707a292e10c871f02a502a56c5e430413fc
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-03-24 16:19:25 +0100

    Added function for links.

---
+++ index.js
@@ -0,0 +1,44 @@
+let links = {};
+
+module.exports = (req, res, next) => {
+    res.link = function() {
+        links[arguments[0]] = {
+                                   href: arguments[1], 
+                                   ...arguments[2] && {meta: {method: arguments[2].toUpperCase()} }
+                               };
+        console.log('LINKS', links);
+    }
+
+    /**
+     * @params {string} gerror - Error message.
+     */
+    res.gerror = function() {
+        let message = (arguments[0]!== null && typeof arguments[0] === 'string') ? arguments[0] : "Error";
+        let data = (typeof arguments[0] === 'object' || typeof arguments[0] === 'array') ? arguments[0] : null;
+        let statusCode = arguments[1] || 400;
+
+        res.status(statusCode).send({error: message, ...data && {data}});
+        return reshelper().res.success();
+    }
+
+    /**
+     * @params {message} - Error message
+     */
+    res.success = function() {
+        let message = (arguments[0]!== null && typeof arguments[0] === 'string') ? arguments[0] : "Operation Successful";
+        
+        let data = (typeof arguments[0] === 'object' || typeof arguments[0] === 'array') ? arguments[0] : 
+                   (typeof arguments[1] === 'object' || typeof arguments[0] === 'array') ? arguments[1] : null;
+        let _links = (Object.keys(links).length > 0) ? links : null;
+		res.send({success: message, ...data && {data}, ..._links && {_links}});
+    }
+
+    res.failure = () => {
+        let message = arguments[0] || "Operation Successful";
+        let statusCode = arguments[1] || 400;
+        let data = arguments[1] || null;
+
+        res.status(statusCode).send({fail: message, ...data && {data}});
+    }
+    next();
+}
---
+++ tests/index.test.js
@@ -0,0 +1,58 @@
+(function () {
+'use strict';
+
+	const express = require('express');
+	const request = require("supertest");
+	const bodyparser = require('body-parser');
+	const responsehelper = require("../index.js");
+	const app = express();
+	app.use(bodyparser.text())
+	app.use(bodyparser.json());
+	app.use(responsehelper);
+
+    app.post('/success/links', function(req, res) {
+        console.info("This routes uses res.link multiple times. This is not the same as Express' res.links");
+        //res.link('/route1');
+        res.link('self', '/route1', 'post');
+        res.link('add_new', '/route/2');
+        return res.success(req.body);
+    });
+	app.post('/success', function(req, res) {
+		return res.success(req.body);
+	});
+
+	describe("Success", function() {
+		test("With Object payload", async function() {
+			let foo = await request(app).post("/success")
+						.send({fee: "fee", fi: "fi!", foh: 'foh!', fum: 'I smell the blood of an Englishman!'});
+            console.log('FOO', foo.body);
+			expect(foo.body).toHaveProperty('success');
+			expect(typeof foo.body.success).toEqual('string');
+			expect(foo.body).toHaveProperty('data');
+		});
+
+		test('res.success(string)', async function() {
+			let payload = 'sample string';
+			let foo = await request(app)
+					.post('/success').set('Content-type', 'text/plain')
+					.send(payload);
+
+			expect(foo.body).toHaveProperty('success', payload);
+		});
+        test('Using res.links multiple times', async function() {
+			let foo = await request(app).post("/success/links").send({fee: "fee", fi: "fi!", foh: 'foh!', fum: 'I smell the blood of an Englishman!'});
+            console.log('FOO STATUS', foo.status, foo.text);
+            console.log('FOO BODY LINKS', foo.body);
+            expect(foo.body).toHaveProperty('_links');
+        });
+		test.skip('String and Object payload', async function() {
+			let payload = 'sample string';
+            let objectpayload = {fee: "fee", fi: "fi!", foh: 'foh!', fum: 'I smell the blood of an Englishman!'};
+			let foo = await request(app)
+					.post('/success').set('Content-type', 'application/json')
+					.send(payload);
+
+			expect(foo.body).toHaveProperty('success', payload);
+		});
+    });
+}());
