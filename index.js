f1c4e9a72e67e3c259fc62435d29e0e2b0e50022
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-03-26 19:30:02 +0100

    Changed structure for error object

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 let links = {};
 
 /*
-console.log('\n--------------\nREQUIRED');
 console.log("NPM PACKAGE.JSON", process.env.npm_package_version);
 console.log('NPM CONFIG ROOT\n-----------------', process.env.npm_config_root);
+let baseUrl = process.env.npm_package_littlezigy-rest;
 */
-//let baseUrl = process.env.npm_package_littlezigy-rest;
 
 module.exports = (req, res, next) => {
-
     res.link = function() {
         links[arguments[0]] = {
-                                   href: arguments[1], 
-                                   ...arguments[2] && {meta: {method: arguments[2].toUpperCase()} },
-                                   ...arguments[3] && {schema: arguments[3]}
-                               };
+           href: arguments[1], 
+           ...arguments[2] && {meta: {method: arguments[2].toUpperCase()} },
+           ...arguments[3] && {schema: arguments[3]}
+        };
     }
 
     /**
      * @params {string} gerror - Error message.
      */
     res.gerror = function() {
-        let message = (arguments[0]!== null && typeof arguments[0] === 'string') ? arguments[0] : "Error";
-        let data = (typeof arguments[0] === 'object' || typeof arguments[0] === 'array') ? arguments[0] : null;
-        let statusCode = arguments[1] || 400;
-
+        let title, code, detail;
+        if(typeof arguments[0] === 'object') {
+            let errobj = arguments[0];
+            title = errobj.title;
+            code = errobj.code;
+            detail = errobj.detail;
+        } else if(typeof arguments[0] === 'string') {
+            if( /[A-Z]/g.test(arguments[0]) ) {
+                title = arguments[0];
+                code = arguments[1] || null;
+            } else if( /[^A-Z]/g.test(arguments[0]) && /[-_]/.test(arguments[0]) ) {
+                code = arguments[0];
+                title = arguments[1] || null;
+            }
+            detail = arguments[2] || null;
+        }
         let _links = (Object.keys(links).length > 0) ? links : null;
-        res.status(statusCode).send({ error: message, ...data && {data}, ..._links && {_links} });
+
+        if(res.statusCode == false || res.statusCode < 400) res.status(400);
+
+        res.send({error: {...code &&  {code}, ...title && {title}, ...detail && {detail}, ..._links && {_links} }});
         links = {};
     }
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -17,15 +17,39 @@
         res.link('add_new', '/route/2');
         return res.success(req.body);
     });
+
 	app.post('/success', function(req, res) {
 		return res.success(req.body);
 	});
+
+    app.post('/error/one/parameter', function(req, res) {
+        console.log("sends error string");
+        return res.gerror(req.body.error);
+	});
+
+    app.post('/error/two/parameter', function(req, res) {
+        return res.gerror(req.body.first, req.body.second);
+    });
+
+    app.post('/error/three/parameter', function(req, res) {
+        return res.gerror(req.body.first, req.body.second, req.body.third);
+    });
+
+    app.post('/error/object', function(req, res) {
+        console.log('sends error object');
+        return res.gerror(req.body); 
+    });
+    app.post('/error/status', function(req, res) {
+        console.log('Sets status code');
+        res.status(req.body.status);
+        return res.gerror({code: 'test_error', title: 'Blah Blah'}); 
+    });
 
 	describe("Success", function() {
 		test("With Object payload", async function() {
 			let foo = await request(app).post("/success")
 						.send({fee: "fee", fi: "fi!", foh: 'foh!', fum: 'I smell the blood of an Englishman!'});
-            console.log('FOO', foo.body);
+
 			expect(foo.body).toHaveProperty('success');
 			expect(typeof foo.body.success).toEqual('string');
 			expect(foo.body).toHaveProperty('data');
@@ -39,22 +63,79 @@
 
 			expect(foo.body).toHaveProperty('success', payload);
 		});
+
         test('Using res.links multiple times', async function() {
 			let foo = await request(app).post("/success/links").send({fee: "fee", fi: "fi!", foh: 'foh!', fum: 'I smell the blood of an Englishman!'});
-            console.log('FOO STATUS', foo.status, foo.text);
-            console.log('FOO BODY LINKS', foo.body);
             expect(foo.body).toHaveProperty('_links');
         });
+
 		test.skip('String and Object payload', async function() {
 			let payload = 'sample string';
             let objectpayload = {fee: "fee", fi: "fi!", foh: 'foh!', fum: 'I smell the blood of an Englishman!'};
+
 			let foo = await request(app)
 					.post('/success').set('Content-type', 'application/json')
 					.send(payload);
 
 			expect(foo.body).toHaveProperty('success', payload);
 		});
-            
+    });
+    
+    describe('Error', function() {
+        test('Sending error title', async function() {
+            let foo = await request(app).post('/error/one/parameter').send({error: 'Fancy Error'});
+            expect(foo.status).toBeGreaterThan(399);
+            expect(foo.body.error).toHaveProperty('title', 'Fancy Error');
+            expect(foo.body.error).not.toHaveProperty('code');
+        });
 
+        test('Sending error code', async function() {
+            let foo = await request(app).post('/error/one/parameter').send({error: 'error_code'});
+            expect(foo.status).toBeGreaterThan(399);
+            expect(foo.body.error).toHaveProperty('code', 'error_code');
+        });
+
+        test('Sending error code first and error title', async function() {
+            let foo = await request(app).post('/error/two/parameter').send({first: 'error_code', second: 'Fancy Error'});
+            expect(foo.status).toBeGreaterThan(399);
+            expect(foo.body.error).toHaveProperty('code', 'error_code');
+            expect(foo.body.error).toHaveProperty('title', 'Fancy Error');
+            expect(foo.body.error).not.toHaveProperty('detail');
+        });
+
+        test('Sending error code first and error title', async function() {
+            let foo = await request(app).post('/error/two/parameter').send({first: 'Fancy Error', second: 'error_code'});
+            expect(foo.status).toBeGreaterThan(399);
+            expect(foo.body.error).toHaveProperty('code', 'error_code');
+            expect(foo.body.error).toHaveProperty('title', 'Fancy Error');
+            expect(foo.body.error).not.toHaveProperty('detail');
+        });
+
+        test('Sending error code, error title, and error detail', async function() {
+            let foo = await request(app).post('/error/three/parameter').send({first: 'Fancy Error', second: 'error_code', third: 'Yummy details'});
+            expect(foo.status).toBeGreaterThan(399);
+            expect(foo.body.error).toHaveProperty('code', 'error_code');
+            expect(foo.body.error).toHaveProperty('title', 'Fancy Error');
+            expect(foo.body.error).toHaveProperty('detail', 'Yummy details');
+        });
+
+        test('Error: Status code 413', async function() {
+            let foo = await request(app).post('/error/status').send({status: 413});
+            expect(foo.status).toEqual(413);
+        });
+
+        test('Error Object with code and title', async function() {
+            let foo = await request(app).post('/error/object').send({code: 'blah_blah', title: 'Professional Blah'});
+            expect(foo.body.error).toHaveProperty('code', 'blah_blah');
+            expect(foo.body.error).toHaveProperty('title', 'Professional Blah');
+            expect(foo.body.error).not.toHaveProperty('detail');
+        });
+        
+        test('Error Object with code and title and detail', async function() {
+            let foo = await request(app).post('/error/object').send({code: 'blah_blah', title: 'Professional Blah', detail: 'Yummy details coming'});
+            expect(foo.body.error).toHaveProperty('code', 'blah_blah');
+            expect(foo.body.error).toHaveProperty('title', 'Professional Blah');
+            expect(foo.body.error).toHaveProperty('detail', 'Yummy details coming');
+        });
     });
 }());
